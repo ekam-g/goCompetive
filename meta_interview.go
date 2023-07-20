e753a5ad52478414d73ac5b603aecbead8931ad5
carghai <carghai74@gmail.com>
2023-07-19 23:42:13 -0400

    meta

--- meta_interview.go
+++ meta_interview.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 func main() {
-	mag := "Meta Platforms, Inc. to Change Ticker Symbol to 'META' on June 9 \nMENLO PARK, Calif., May 31, 2022 /PRNewswire/ -- Meta Platforms, Inc. (Nasdaq: FB) today announced that its Class A common stock will begin trading on NASDAQ under the ticker symbol 'META' prior to market open on June 9, 2022. This will replace the company's current ticker symbol 'FB', which has been used since its initial public offering in 2012. The new ticker symbol aligns with the company's rebranding from Facebook to Meta, announced on October 28, 2021."
+	mag := "Meta Platforms, Inc. to Change Ticker Symbol to  'META' on June 9 \nMENLO PARK, Calif., May 31, 2022 /PRNewswire/ -- Meta Platforms, Inc. (Nasdaq: FB) today announced that its Class A common stock will begin trading on NASDAQ under the ticker symbol 'META' prior to market open on June 9, 2022. This will replace the company's current ticker symbol 'FB', which has been used since its initial public offering in 2012. The new ticker symbol aligns with the company's rebranding from Facebook to Meta, announced on October 28, 2021."
 	letters := make(map[byte]int8)
 	mag = strings.ToLower(mag)
 	mag = strings.ReplaceAll(mag, ".", "")
